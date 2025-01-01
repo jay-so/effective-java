97a372cf6d3462a4c2eb44bdb3770f1e3853e1e7
jay-so <merry9504@naver.com>
2025-01-01 12:09:18 +0900

    docs: Item2 문서 작성

---
+++ Item2/code/hierarchicalbuilder/PizzaTest.java
@@ -0,0 +1,16 @@
+package hierarchicalbuilder;
+import static hierarchicalbuilder.Pizza.Topping.*;
+import static hierarchicalbuilder.NyPizza.Size.*;
+
+
+public class PizzaTest {
+    public static void main(String[] args) {
+        NyPizza pizza = new NyPizza.Builder(SMALL)
+                .addTopping(SAUSAGE).addTopping(ONION).build();
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(HAM).sauceInside().build();
+
+        System.out.println(pizza);
+        System.out.println(calzone);
+    }
+}
